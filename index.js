5c3fe8662a257d57541fdfff0704fb7f743359de
Jean Yang <bdlas321@gmail.com>
2018-11-22 17:38:28 +0800

    feat(getKey): get key from object or json file

---
+++ index.js
@@ -0,0 +1,26 @@
+/* REF:
+* https://developers.google.com/identity/protocols/OAuth2ServiceAccount
+* https://developers.google.com/identity/protocols/googlescopes
+*/
+
+const fs = require('fs');
+
+exports.getKey = function getKey(pathOrObject) {
+  let key;
+  if (typeof pathOrObject === 'string') {
+    key = JSON.parse(fs.readFileSync(pathOrObject, 'utf8'));
+  } else if (typeof pathOrObject === 'object') {
+    key = pathOrObject;
+  } else {
+    throw Error('no google cloud service-account key found!');
+  }
+  if (!key || key.type !== 'service_account' || !key.project_id || !key.private_key || !key.client_email) {
+    throw Error('key is invalid!');
+  }
+  return key;
+}
+
+exports.default = function getAccessToken(pathOrObject, scope) {
+  const key = getKey(pathOrObject);
+  return key;
+}
---
+++ test/getKey.test.js
@@ -0,0 +1,28 @@
+const { expect } = require('chai');
+const getKey = require('../index').getKey;
+
+describe('getKey', function () {
+  describe('it should parse JSON key file correctly', function () {
+    const key = getKey('./test/gckey.example.json');
+    it('should return an object', function () {
+      expect(key).to.be.an('object');
+    });
+    it('should parse necessary fields', function () {
+      expect(key).to.include.all.keys('type', 'client_email', 'project_id', 'private_key');
+    });
+    it('should be service account', function () {
+      expect(key.type).to.equal('service_account');
+    });
+  });
+  describe('it should check key object', function() {
+    it('should throw error if some fields missing', function () {
+      expect(() => getKey({})).to.throw(Error, 'key is invalid!');
+      expect(() => getKey({ type: 'aaa' })).to.throw(Error, 'key is invalid!');
+      expect(() => getKey({ type: 'service_account' })).to.throw(Error, 'key is invalid!');
+      expect(() => getKey({ type: 'service_account', project_id: 'abc-123' })).to.throw(Error, 'key is invalid!');
+      expect(() => getKey({ type: 'service_account', project_id: 'abc-123', private_key: '-----BEGIN PRIVATE KEY-----\nMIIE' })).to.throw(Error, 'key is invalid!');
+      expect(() => getKey({ type: 'service_account', project_id: 'abc-123', private_key: '-----BEGIN PRIVATE KEY-----\nMIIE', client_email: '' })).to.throw(Error, 'key is invalid!');
+      expect(() => getKey({ type: 'service_account', project_id: 'abc-123', private_key: '-----BEGIN PRIVATE KEY-----\nMIIE', client_email: 'user-name@gcp-project-1245.iam.gserviceaccount.com' })).to.not.throw(Error, 'key is invalid!');
+    });
+  });
+});
